c0b16d0accecf3dd23ecf7e3c80ee46ab4979cba
yuppieyay48-commits <yuppieyay48@gmail.com>
2026-09-20 18:50:33 +0200

    Update URL for 'Steal An Egg' game

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [10563114921] = "", -- Steal An Egg
+  [10563114921] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sae.lua", -- Steal An Egg
 }
 
 return Games
