f736c7f17f8954badc340da8d6401ebb9be4bfc6
yuppieyay48-commits <yuppieyay48@gmail.com>
2026-09-20 21:18:55 +0200

    Update game URL for ID 109983668079237

--- gamelist.lua
+++ gamelist.lua
@@ -1,6 +1,6 @@
 local Games = {
   [10563114921] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sae.lua",  -- Steal An Egg
-  [109983668079237] = "",
+  [109983668079237] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sab.lua",
 }
 
 return Games
