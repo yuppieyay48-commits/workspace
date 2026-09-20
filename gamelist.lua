b26397242a7df1d837119fd8ccf03a869262380c
yuppieyay48-commits <yuppieyay48@gmail.com>
2026-09-20 21:17:55 +0200

    Fix formatting in gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [10563114921] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sae.lua", -- Steal An Egg
+  [10563114921] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sae.lua",  -- Steal An Egg
+  [109983668079237] = "",
 }
 
 return Games
