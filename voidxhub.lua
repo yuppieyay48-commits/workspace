8c99ecf330cd01f88b04499d167b877ab1293519
yuppieyay48-commits <yuppieyay48@gmail.com>
2026-09-20 18:31:23 +0200

    Load game scripts based on GameId

---
+++ voidxhub.lua
@@ -0,0 +1,11 @@
+local HttpGet = game.HttpGet
+local GameId: number = game.GameId
+
+local Games: {[number]: string} = loadstring(
+  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+)() :: any
+
+local URL: string? = Games[GameId]
+if not URL then return end
+
+loadstring(HttpGet(game, URL))()
