0c288b65b7b65eb42adf539d6d1160cebcc4a52b
yuppieyay48-commits <yuppieyay48@gmail.com>
2026-09-21 17:48:03 +0200

    Add new game URLs to gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,6 +1,7 @@
 local Games = {
   [10563114921] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sae.lua",  -- Steal An Egg
-  [109983668079237] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sab.lua",
+  [109983668079237] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/sab.lua", -- Steal a brainrot
+  [17625359962] = "https://raw.githubusercontent.com/yuppieyay48-commits/workspace/refs/heads/main/rivals.lua", -- Rivals
 }
 
 return Games
